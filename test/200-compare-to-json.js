58ad6a481a1e4ac41dc410a91938dc7a10588899
Florent Jaby <fjaby@octo.com>
2014-10-30 17:03:04 +0100

    add test for arrays

--- test/200-compare-to-json.js
+++ test/200-compare-to-json.js
@@ -17,6 +17,7 @@ describe('stringify', function () {
     describe('with strings', compares(['a', 'b', 'c']))
     describe('with mixed scalar values', compares([true, null, 'ABCD', 89]))
     describe('containing another array', compares([true, [], 2]))
+    describe('containing another array as first item', compares([[true, 18], 2]))
   });
   describe('given an empty object', compares({}))
   describe('given an object', function () {
